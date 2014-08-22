f1ffc7e16cb18e35144f4ea9b31f97eaa8468d8b
Charles Smith <twohlix@gmail.com>
2014-08-21 21:50:13 -0400

    Reduced search space on palindrome checker. Thanks @jts3

--- palindrome.py
+++ palindrome.py
@@ -22,12 +22,33 @@ for i in range(1, theNumber):
  	minN *= 10	
 maxN -= 1
 
-candidates = []
-for i in range(maxN, minN, -1):
-	for j in range(maxN, minN, -1):
-		if isPalindrome( i * j ):
-			candidates.append(i*j)
 
-winner = max(candidates)
-print winner
+def optimizedCheck(minN, maxN):
+	range_list = range(maxN, minN, -1)
+	count = 0
+	candidates = []
+	for i in range_list:
+		for j in range_list:
+			count += 1
+			if isPalindrome( i*j ):
+				candidates.append(i*j)
+		range_list.pop(0)
+
+	print "Optimized:", count
+	print max(candidates)
+
+
+def bruteCheck(minN, maxN):
+	count = 0
+	candidates = []
+	for i in range(maxN, minN, -1):
+		for j in range(maxN, minN, -1):
+			count += 1
+			if isPalindrome( i * j ):
+				candidates.append(i*j)
+	print "Brute:", count
+	print max(candidates)
+
+optimizedCheck(minN, maxN)
+bruteCheck(minN, maxN)
 
